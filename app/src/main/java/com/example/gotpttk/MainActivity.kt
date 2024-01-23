c7b2f004f40797ff549b419b2aa7d26fc5a9b556
Kacper Gaudyn <kacpergaudyn04@gmail.com>
2024-01-23 19:19:51 +0100

    Subbuttons added

--- app/src/main/java/com/example/gotpttk/MainActivity.kt
+++ app/src/main/java/com/example/gotpttk/MainActivity.kt
@@ -7,18 +7,16 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
-import androidx.compose.material3.Divider
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.ModalDrawerSheet
-import androidx.compose.material3.ModalNavigationDrawer
-import androidx.compose.material3.NavigationDrawerItem
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
@@ -32,12 +30,11 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             GOTPTTKTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting()
+                    LandingPage()
                 }
             }
         }
@@ -45,43 +42,74 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting() {
-    Column (
+fun LandingPage() {
+    var expandedButton by remember {
+        mutableStateOf("")
+    }
+    val buttons: Map<String, Map<String, () -> Unit>> = mapOf(
+        "Moja książeczka" to mapOf(
+            "Punkty" to {},
+            "Inne" to {},
+        ),
+        "Wędrówki" to mapOf(
+            "Planuj" to {},
+            "Jeszcze inne" to {},
+            "Idk" to {}
+        )
+    )
+    Column(
         modifier = Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally
-        ) {
+    ) {
         Spacer(modifier = Modifier.height(20.dp))
-        Text(text = "E-KSIĄŻECZKA",
+        Text(
+            text = "E-KSIĄŻECZKA",
             fontSize = 40.sp
         )
-        Text(text = "GOT PTTK",
+        Text(
+            text = "GOT PTTK",
             fontSize = 50.sp,
-            fontWeight = FontWeight.Bold)
-        Spacer(modifier = Modifier.height(20.dp))
-        Button(
-            onClick = { /*TODO*/ },
-            modifier = Modifier
-                .height(80.dp)
-                .width(300.dp)) {
-            Text(text = "Moja książeczka",
-                fontSize = 30.sp)
-        }
-        Spacer(modifier = Modifier.height(20.dp))
-        Button(
-            onClick = { /*TODO*/ },
-            modifier = Modifier
-                .height(80.dp)
-                .width(300.dp)) {
-            Text(text = "Wędrówki",
-                fontSize = 30.sp)
+            fontWeight = FontWeight.Bold
+        )
+        buttons.forEach { (buttonName, subButtons) ->
+            Spacer(modifier = Modifier.height(20.dp))
+            Button(
+                onClick = { expandedButton = if (expandedButton == buttonName) "" else buttonName },
+                modifier = Modifier
+                    .height(80.dp)
+                    .width(300.dp)
+            ) {
+                Text(
+                    text = buttonName,
+                    fontSize = 30.sp
+                )
+            }
+            if (expandedButton == buttonName) {
+                Column {
+                    subButtons.forEach { (subButtonName, function) ->
+                        Spacer(modifier = Modifier.height(10.dp))
+                        Button(
+                            onClick = function,
+                            modifier = Modifier
+                                .height(60.dp)
+                                .width(200.dp)
+                        ) {
+                            Text(
+                                text = subButtonName,
+                                fontSize = 20.sp
+                            )
+                        }
+                    }
+                }
+            }
         }
     }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun LandingPagePreview() {
     GOTPTTKTheme {
-        Greeting()
+        LandingPage()
     }
 }
