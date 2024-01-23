4413618d49ff301e8c7345705cb752276b726453
Kacper Gaudyn <kacpergaudyn04@gmail.com>
2024-01-23 18:02:19 +0100

    Landing page init frontend.

--- app/src/main/java/com/example/gotpttk/MainActivity.kt
+++ app/src/main/java/com/example/gotpttk/MainActivity.kt
@@ -3,13 +3,28 @@ package com.example.gotpttk
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
+import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.NavigationDrawerItem
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.gotpttk.ui.theme.GOTPTTKTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +37,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Greeting()
                 }
             }
         }
@@ -30,17 +45,43 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Greeting() {
+    Column (
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+        Spacer(modifier = Modifier.height(20.dp))
+        Text(text = "E-KSIĄŻECZKA",
+            fontSize = 40.sp
+        )
+        Text(text = "GOT PTTK",
+            fontSize = 50.sp,
+            fontWeight = FontWeight.Bold)
+        Spacer(modifier = Modifier.height(20.dp))
+        Button(
+            onClick = { /*TODO*/ },
+            modifier = Modifier
+                .height(80.dp)
+                .width(300.dp)) {
+            Text(text = "Moja książeczka",
+                fontSize = 30.sp)
+        }
+        Spacer(modifier = Modifier.height(20.dp))
+        Button(
+            onClick = { /*TODO*/ },
+            modifier = Modifier
+                .height(80.dp)
+                .width(300.dp)) {
+            Text(text = "Wędrówki",
+                fontSize = 30.sp)
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     GOTPTTKTheme {
-        Greeting("Android")
+        Greeting()
     }
 }
